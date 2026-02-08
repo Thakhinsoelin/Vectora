60eac457da40c5cb2a86590f26c34c56650016ac
soethiha <usersoe123@gmail.com>
2026-02-08 18:06:58 +0630

    to default

--- Vectora_Engine/Application.cpp
+++ Vectora_Engine/Application.cpp
@@ -25,20 +25,20 @@ namespace Vectora {
 		VE_CORE_ASSERT(!s_Instance, "Application already exists!");
 		s_Instance = this;
 
-		window = std::unique_ptr<Window>(Window::Create(WindowProps("test",800, 800 )));
+		window = std::unique_ptr<Window>(Window::Create(/*WindowProps("test",800, 800 ))*/));
 		window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 		m_ImguiLayer = new ImGuiLayer();
 		PushOverlay(m_ImguiLayer);
 
-		/*m_Shader = std::make_shared<Shader>("shaders/vertex.glsl", "shaders/fragment.glsl");
+		m_Shader = std::make_shared<Shader>("shaders/vertex.glsl", "shaders/fragment.glsl");
 		m_Shader->createShaders(BOTH_FROM_FILE);
-		m_Shader->Bind();*/
+		m_Shader->Bind();
 		
-		/*m_BlueShader = std::make_shared<Shader>("shaders/blueRectVt.glsl", "shaders/blueRectFg.glsl");
-		m_BlueShader->createShaders(BOTH_FROM_FILE);*/
+		m_BlueShader = std::make_shared<Shader>("shaders/blueRectVt.glsl", "shaders/blueRectFg.glsl");
+		m_BlueShader->createShaders(BOTH_FROM_FILE);
 		
 
-		/*float vertices[] = {
+		float vertices[] = {
 			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
 			 0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
 			 0.0f,  0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
@@ -81,16 +81,16 @@ namespace Vectora {
 		uint32_t squareIndices[6] = { 0, 1, 2, 2, 3, 0 };
 		std::shared_ptr<IndexBuffer> squareIB;
 		squareIB.reset(IndexBuffer::Create(squareIndices, sizeof(squareIndices) / sizeof(uint32_t)));
-		m_SquareVA->SetIndexBuffer(squareIB);*/
+		m_SquareVA->SetIndexBuffer(squareIB);
 		
 
-		SagA = BlacHole(glm::vec3(0.f, 0.f, 0.f), 8.54e36f);
+		/*SagA = BlacHole(glm::vec3(0.f, 0.f, 0.f), 8.54e36f);
 
 		for (float i = -1.f; i <= 1.f; i += 0.05f)
 		{
 			rays.push_back(Ray(glm::vec2(-0.5f, i), glm::vec2(1.0f, 0.f)));
 
-		}
+		}*/
 
 	}
 
@@ -107,29 +107,42 @@ namespace Vectora {
 			glClearColor(0.1, 0.1, 0.1, 1);
 			glClear(GL_COLOR_BUFFER_BIT);
 
-			//m_BlueShader->Bind();
-			//m_SquareVA->Bind();
-			//glDrawElements(GL_TRIANGLES, m_SquareVA->GetIndexBuffer()->GetCount(), GL_UNSIGNED_INT, nullptr);
-			///*glDrawArrays(GL_TRIANGLES, 0, 3);*/
-			//
-			//m_Shader->Bind();
-			//m_VertexArray->Bind();
-			//glDrawElements(GL_TRIANGLES, m_VertexArray->GetIndexBuffer()->GetCount(), GL_UNSIGNED_INT, nullptr);
+			m_BlueShader->Bind();
+			m_SquareVA->Bind();
+			glDrawElements(GL_TRIANGLES, m_SquareVA->GetIndexBuffer()->GetCount(), GL_UNSIGNED_INT, nullptr);
+			/*glDrawArrays(GL_TRIANGLES, 0, 3);*/
 			
-			/*glDrawArrays(GL_LINE_LOOP, 0,m_VertexArray->GetVertexBuffers().size());*/
-			SagA.draw();
+			m_Shader->Bind();
+			m_VertexArray->Bind();
+			glDrawElements(GL_TRIANGLES, m_VertexArray->GetIndexBuffer()->GetCount(), GL_UNSIGNED_INT, nullptr);
+			
+			
+			//SagA.draw();
 
-			for (auto& e : rays) {
-				//glLineWidth(3.0f);
-				e.draw();
-				//e.step(SagA.radius);
-				e.gpt_step(SagA.radius, glm::vec2(SagA.position));
-			}
+			//for (auto& e : rays) {
+			//	//glLineWidth(3.0f);
+			//	e.draw();
+			//	//e.step(SagA.radius);
+			//	e.gpt_step(SagA.radius, glm::vec2(SagA.position));
+			//}
 
 			for (auto& layer : layerstack)
 				layer->OnUpdate();
 			
+			
 			m_ImguiLayer->Begin();
+			//ImGui::Begin("bla");
+			//ImGui::Text("Hello World");
+			//if (ImGui::Button("Restart")) {
+			//	int counter = 0;
+			//	for (float i = -1.f; i <= 1.f; i += 0.05f)
+			//	{
+			//		//rays.push_back(Ray(glm::vec2(-0.5f, i), glm::vec2(1.0f, 0.f)));
+			//		rays[counter] = Ray(glm::vec2(-0.5f, i), glm::vec2(1.0f, 0.f));
+			//		counter++;
+			//	}
+			//};
+			//ImGui::End();
 			for (Layer* layer: layerstack)
 			{
 				layer->OnImGuiRender();
