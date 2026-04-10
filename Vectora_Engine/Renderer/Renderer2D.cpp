7d1814afd4b4bdfdf208918ce75bf952e876303c
soethiha <usersoe123@gmail.com>
2026-04-10 18:14:11 +0630

    Needs fixing

--- Vectora_Engine/Renderer/Renderer2D.cpp
+++ Vectora_Engine/Renderer/Renderer2D.cpp
@@ -32,7 +32,7 @@ namespace Vectora {
 	struct LineVertex
 	{
 		glm::vec3 Position;
-		float Padding = 0.0f; // Manually fill the 4-byte hole
+		//float Padding = 0.0f; // Manually fill the 4-byte hole
 		glm::vec4 Color;
 
 		// Editor-only
