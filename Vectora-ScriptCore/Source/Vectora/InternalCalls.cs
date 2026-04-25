e5623f9403a0eb6283c73a3978618e63540bb2fc
ThaKhinSo <usersoe123@gmail.com>
2026-04-26 00:05:28 +0630

    starting out

---
+++ Vectora-ScriptCore/Source/Vectora/InternalCalls.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Vectora
+{
+    public static class InternalCalls
+    {
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        internal extern static void Entity_GetTranslation(ulong entityID, out Vector3 translation);
+
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        internal extern static void Entity_SetTranslation(ulong entityID, ref Vector3 translation);
+
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        internal extern static bool Input_IsKeyDown(KeyCodes keycode);
+    }
+}
---
+++ Vectora-ScriptCore/Source/Vectora/Vector3.cs
@@ -0,0 +1,31 @@
+﻿namespace Vectora
+{
+    public struct Vector3
+    {
+        public float X;
+        public float Y;
+        public float Z;
+
+        public static Vector3 Zero => new Vector3(0, 0, 0);
+
+        public Vector3(float scalar)
+        {
+            X = scalar;
+            Y = scalar;
+            Z = scalar;
+        }
+        public Vector3(float x, float y, float z)
+        {
+            X = x; Y = y; Z = z;
+        }
+        public static Vector3 operator *(Vector3 v1, float scalar)
+        {
+            return new Vector3(v1.X * scalar, v1.Y * scalar, v1.Z * scalar);
+        }
+
+        public static Vector3 operator +(Vector3 v1, Vector3 v2)
+        {
+            return new Vector3(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
+        }
+    }
+}
